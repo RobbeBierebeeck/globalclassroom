57dd0f039579acb7d36fdd500cd45c9f27380645
robbe.bierebeeck4@gmail.com <oAqhGMbYF9CBf3>
2022-10-27 22:45:17 +0200

    initial setup

---
+++ partials/navigation.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Cities </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Code</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link">Survey</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
