79701adf4677c001c054fd6035168c31260e3b25
Seppe Vangeel <seppe.vg@live.be>
2022-11-09 19:08:26 +0100

    Feat: Start city.php & import img

---
+++ city.php
@@ -0,0 +1,264 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php include_once(__DIR__ . '/style.php') ?>
+    <title>Our City | Global Classroom</title>
+</head>
+
+<body>
+
+    <header>
+        <?php include_once(__DIR__ . '/partials/navigation.php') ?>
+    </header>
+
+    <main>
+
+        <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="4" aria-label="Slide 5"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="5" aria-label="Slide 6"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="6" aria-label="Slide 7"></button>
+                <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="7" aria-label="Slide 8"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="./assets/moodboard.jpeg" alt="Our Moodboard" width="100%" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
+
+                    <div class="container">
+                        <div class="carousel-caption">
+                            <p><a target="_blank" rel="noopener noreferrer" class="btn btn-lg btn-primary" href="./assets/moodboard.jpeg">See our moodboard</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="./assets/Moodboard_Alan.jpeg" alt="Our Moodboard" width="100%" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
+
+                    <div class="container">
+                        <div class="carousel-caption">
+                            <p><a target="_blank" rel="noopener noreferrer" class="btn btn-lg btn-primary" href="./assets/Moodboard_Alan.jpeg">See Alan's moodboard</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="./assets/Moodboard_Ismaïl.jpeg" alt="Our Moodboard" width="100%" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
+
+
+                    <div class="container">
+                        <div class="carousel-caption">
+                            <p><a target="_blank" rel="noopener noreferrer" class="btn btn-lg btn-primary" href="./assets/Moodboard_Ismaïl.jpeg">See Ismaïl's moodboard</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="./assets/Moodboard_Macarena.jpeg" alt="Our Moodboard" width="100%" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
+
+
+                    <div class="container">
+                        <div class="carousel-caption">
+                            <p><a target="_blank" rel="noopener noreferrer" class="btn btn-lg btn-primary" href="./assets/Moodboard_Macarena.jpeg">See Macarena's moodboard</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="./assets/Moodboard_Nicole.jpeg" alt="Our Moodboard" width="100%" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
+
+
+                    <div class="container">
+                        <div class="carousel-caption">
+                            <p><a target="_blank" rel="noopener noreferrer" class="btn btn-lg btn-primary" href="./assets/Moodboard_Nicole.jpeg">See Nicole's moodboard</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="./assets/Moodboard_Robbe.jpeg" alt="Our Moodboard" width="100%" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
+
+
+                    <div class="container">
+                        <div class="carousel-caption">
+                            <p><a target="_blank" rel="noopener noreferrer" class="btn btn-lg btn-primary" href="./assets/Moodboard_Robbe.jpeg">See Robbe's moodboard</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="./assets/Moodboard_Seppe.png" alt="Our Moodboard" width="100%" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
+
+
+                    <div class="container">
+                        <div class="carousel-caption">
+                            <p><a target="_blank" rel="noopener noreferrer" class="btn btn-lg btn-primary" href="./assets/Moodboard_Seppe.png">See Seppe's moodboard</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="./assets/Moodboard_William.jpeg" alt="Our Moodboard" width="100%" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
+
+
+                    <div class="container">
+                        <div class="carousel-caption">
+                            <p><a target="_blank" rel="noopener noreferrer" class="btn btn-lg btn-primary" href="./assets/Moodboard_William.jpeg">See William's moodboard</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+
+
+        <div class="container marketing">
+            <h3 class="text-center m-5">
+                This is the ideal city
+                <small class="text-muted">- How we like it</small>
+            </h3>
+            <div class="row">
+                <div class="col-lg-4">
+                    <svg class="bd-placeholder-img rounded-circle" width="100" height="100" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 100x100" preserveAspectRatio="xMidYMid slice" focusable="false">
+                        <title>Compact</title>
+                        <rect width="100%" height="100%" fill="#8064A2" />
+                    </svg>
+
+                    <h2 class="fw-normal">Compact</h2>
+                    <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
+                </div>
+                <div class="col-lg-4">
+                    <svg class="bd-placeholder-img rounded-circle" width="100" height="100" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 100x100" preserveAspectRatio="xMidYMid slice" focusable="false">
+                        <title>Urban Space Appropiation</title>
+                        <rect width="100%" height="100%" fill="#8064A2" />
+                    </svg>
+
+                    <h2 class="fw-normal">Urban Space Appropiation</h2>
+                    <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
+                </div>
+                <div class="col-lg-4">
+                    <svg class="bd-placeholder-img rounded-circle" width="100" height="100" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 100x100" preserveAspectRatio="xMidYMid slice" focusable="false">
+                        <title>Smart City</title>
+                        <rect width="100%" height="100%" fill="#8064A2" />
+                    </svg>
+
+                    <h2 class="fw-normal">Smart City</h2>
+                    <p>And lastly this, the third column of representative placeholder content.</p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-4">
+                    <svg class="bd-placeholder-img rounded-circle" width="100" height="100" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 100x100" preserveAspectRatio="xMidYMid slice" focusable="false">
+                        <title>Accesible</title>
+                        <rect width="100%" height="100%" fill="#8064A2" />
+                    </svg>
+
+                    <h2 class="fw-normal">Accesible</h2>
+                    <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
+                </div>
+                <div class="col-lg-4">
+                    <svg class="bd-placeholder-img rounded-circle" width="100" height="100" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 100x100" preserveAspectRatio="xMidYMid slice" focusable="false">
+                        <title>Major Walking Routes</title>
+                        <rect width="100%" height="100%" fill="#8064A2" />
+                    </svg>
+
+                    <h2 class="fw-normal">Major Walking Routes</h2>
+                    <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
+                </div>
+                <div class="col-lg-4">
+                    <svg class="bd-placeholder-img rounded-circle" width="100" height="100" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 100x100" preserveAspectRatio="xMidYMid slice" focusable="false">
+                        <title>Vertical Gardens</title>
+                        <rect width="100%" height="100%" fill="#8064A2" />
+                    </svg>
+
+                    <h2 class="fw-normal">Vertical Gardens</h2>
+                    <p>And lastly this, the third column of representative placeholder content.</p>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-4">
+                    <svg class="bd-placeholder-img rounded-circle" width="100" height="100" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 100x100" preserveAspectRatio="xMidYMid slice" focusable="false">
+                        <title>Orthogonal Urban Layout</title>
+                        <rect width="100%" height="100%" fill="#8064A2" />
+                    </svg>
+
+                    <h2 class="fw-normal">Orthogonal Urban Layout</h2>
+                    <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
+                </div>
+                <div class="col-lg-4">
+                    <svg class="bd-placeholder-img rounded-circle" width="100" height="100" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 100x100" preserveAspectRatio="xMidYMid slice" focusable="false">
+                        <title>Good Public Transportation</title>
+                        <rect width="100%" height="100%" fill="#8064A2" />
+                    </svg>
+
+                    <h2 class="fw-normal">Good Public Transportation</h2>
+                    <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
+                </div>
+            </div>
+
+
+            <!-- START THE FEATURETTES -->
+
+            <hr class="featurette-divider">
+
+            <div class="row featurette">
+                <div class="col-md-7">
+                    <h2 class="featurette-heading fw-normal lh-1">Public Life lesson</h2>
+                    <p class="lead">During the lesson, we had to think about the road to school, and how we felt about it. For this, we made a quick sketch and had to indicate things we were thinking of such as trees, parks, churches, ...</p>
+                </div>
+                <div class="col-md-5">
+                    <img src="./assets/Home-School.png" alt="Map to School" class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false">
+                </div>
+            </div>
+
+            <hr class="featurette-divider">
+
+            <div class="row featurette">
+                <div class="col-md-7 order-md-2">
+                    <h2 class="featurette-heading fw-normal lh-1">The Image of the City <span class="text-muted">by Kevin Lynch</span></h2>
+                    <p class="lead">During the lesson on "The Image of the City", we learnt another way of looking at cities. We did this with the help of Kevin Lynch's book. Lynch introduces and describes five elements - nodes, paths, districts, landmarks and edges.</p>
+                </div>
+                <div class="col-md-5 order-md-1">
+                    <img src="./assets/KevinLynch.png" alt="Elements of a city" class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false">
+                </div>
+            </div>
+
+            <hr class="featurette-divider">
+
+            <div class="row featurette">
+                <div class="col-md-7">
+                    <h2 class="featurette-heading fw-normal lh-1">The Elements of Mechelen</h2>
+                    <p class="lead">Of course, our new skills had to be tested. So the Mechelen students were now tasked with applying Lynch's theory to the city of Mechelen. Or at least as well as possible ;)</p>
+                    <p><a target="_blank" rel="noopener noreferrer" href="./assets/Mechelen_Kevin_Lynch.pdf" class="btn btn-secondary">See full exercise</a></p>
+                </div>
+                <div class="col-md-5">
+                    <img src="./assets/Mechelen_Kevin_Lynch.jpg" alt="Elements of Mechelen" class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false">
+                </div>
+            </div>
+
+            <hr class="featurette-divider">
+
+            <!-- /END THE FEATURETTES -->
+        </div>
+
+        <footer class="container">
+            <p class="float-end"><a href="#" class="btn btn-primary">Back to top</a></p>
+            <p>&copy; 2022 Global CLassroom &middot; Nicole &middot; Alan &middot; Macarena &middot; Erick &middot; Robbe &middot; Ismaïl &middot; William &middot; Seppe</p>
+        </footer>
+
+    </main>
+
+    <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
--- partials/navigation.php
+++ partials/navigation.php
@@ -1,18 +1,32 @@
-<nav class="navbar navbar-expand-lg bg-light">
+<?php $pageName = basename($_SERVER['PHP_SELF']); ?>
+<nav class="navbar navbar-expand-md navbar-light fixed-top bg-light">
     <div class="container-fluid">
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
+        <a class="navbar-brand" href="./index.php">Global Classroom</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav me-auto mb-2 mb-md-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link 
+                    <?php if ($pageName == "index.php") {
+                        echo "active";
+                    } ?>" aria-current="page" href="./index.php">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Cities </a>
+                    <a class="nav-link <?php if ($pageName == "city.php") {
+                                            echo "active";
+                                        } ?>" href="./city.php">City</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Code</a>
+                    <a class="nav-link <?php if ($pageName == "code.php") {
+                                            echo "active";
+                                        } ?>" href="./code.php"">Code</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link">Survey</a>
+                <li class=" nav-item">
+                        <a class="nav-link <?php if ($pageName == "survey.php") {
+                                                echo "active";
+                                            } ?>" href="./survey.php"">Survey</a>
                 </li>
             </ul>
         </div>
